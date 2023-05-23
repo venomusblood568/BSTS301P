3514a7c3cdc50bfa541972de72024b8bf14d7e1c
Gourav anand <90522738+venomusblood568@users.noreply.github.com>
2023-05-23 23:46:18 +0530

    Update bitwise_palindrone.java

--- palindrome/bitwise_palindrone.java
+++ palindrome/bitwise_palindrone.java
@@ -25,3 +25,31 @@ class main {
         }
     }
 }
+
+/*
+The given code is a Java program that checks whether a given number is a palindrome in its binary representation. Here's a line-by-line explanation of the code:
+
+1. `class main {`: This line declares a class named "main".
+
+3. `public static boolean bit_palindrone(int n) {`: This line defines a public static method named "bit_palindrome" that takes an integer argument "n" and returns a boolean value. The purpose of this method is to check whether the binary representation of "n" is a palindrome.
+
+5. `int rev = 0;`: This line initializes an integer variable "rev" to 0. This variable will store the reverse of the binary representation of "n".
+
+7. `int k = n;`: This line initializes an integer variable "k" with the value of "n". This variable will be used to process each bit of "n" in a loop.
+
+9. `while (k > 0) {`: This line starts a while loop that will iterate until "k" becomes 0. It processes each bit of "n" in each iteration.
+
+11. `rev = (rev << 1) | (k & 1);`: This line performs the reversal of bits. It shifts the bits of "rev" to the left by 1 position and then ORs it with the rightmost bit of "k" (obtained using bitwise AND operation with 1). This operation effectively adds the rightmost bit of "k" to the reversed "rev".
+
+12. `k = k >> 1;`: This line shifts the bits of "k" to the right by 1 position, effectively dropping the rightmost bit that was processed in the previous step.
+
+15. `return n == rev;`: This line checks whether the original value of "n" is equal to the reversed value "rev" and returns a boolean result indicating whether they are equal. If they are equal, it means the binary representation of "n" is a palindrome.
+
+19. `public static void main(String args[]) {`: This line defines the main method, which serves as the entry point of the program. It takes an array of strings as an argument.
+
+21. `int n = 10;`: This line initializes an integer variable "n" with the value 10. This will be the input number for which we want to check if its binary representation is a palindrome.
+
+23-27. These lines contain a conditional statement (if-else) that checks the result of the "bit_palindrome" method for the number "n". If the result is true, it prints that "n is palindrome," otherwise it prints that "n is not palindrome."
+
+Overall, this code checks whether the binary representation of the input number "n" is a palindrome by reversing its bits and comparing it to the original number.
+*/
